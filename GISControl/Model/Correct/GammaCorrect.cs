db56b94bf97ff98f79bd379cc985e278599e3059
TigerBOX <darkuslension@gmail.com>
2020-06-24 19:59:55 +0700

    fix gamma

--- GISControl/Model/Correct/GammaCorrect.cs
+++ GISControl/Model/Correct/GammaCorrect.cs
@@ -6,7 +6,7 @@ namespace GISControl.Model.Correct
     {
         protected override byte GetCorrectPixel(byte pixelColor, double value)
         {
-            int color = Math.Min(255, (int)((255.0 * Math.Pow(pixelColor / 255.0, 1.0 / value)) + 0.5));
+            double color = 255.0 * Math.Pow(pixelColor / 255.0, 1.0 / value);
 
             if (color < 0) color = 0;
             if (color > 255) color = 255;
